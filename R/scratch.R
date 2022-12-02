4104a0042f2b15fbd3b95341489289897c1b9162
Hayden Atchley <atchley.sha@gmail.com>
2022-12-02 10:49:35 -0700

    new data

--- R/scratch.R
+++ R/scratch.R
@@ -1,48 +1,29 @@
-pacman::p_load(pacman, tidyverse, ggthemes, GGally, broom)
+pacman::p_load(pacman, tidyverse, ggthemes, GGally, broom) 
 
-crash <- read_csv("data/TIM 2018 and 2022 Data.csv") %>% 
-  mutate(`Crash Type` = case_when(
-    `Crash Type` == "PD crash" ~ "PD Crash",
-    `Crash Type` == "PI crash" ~ "PI Crash",
-    TRUE ~ `Crash Type`),
-    Year = as.character(Year),
-    `RCT for IMT` = `RCT for IMT` %>% as.numeric(),
-    `Response Time IMT` = `Response Time IMT` %>% 
-      as.difftime() %>% 
-      as.numeric(),
-    Time = Time %>% as.numeric()
-  ) 
+crashfile <- read_csv("data/TIM 2018 and 2022 Data mod.csv") 
 
-crash
-%>% 
-  select(Year, Time, `Crash Type`, `Number of IMT Teams`, `Response Time IMT`, `RCT for IMT`, `Lanes at Bottleneck`, `Number of Lanes Closed`, `Affected Volume`, `Total Excess Travel Time`) %>%
-  filter(`Total Excess Travel Time` > 0)
+crash <- crashfile %>% 
+  transmute(
+    year = as.factor(Year),
+    type = as.factor(`Crash Type`),
+    teams = as.integer(`Number of IMT Teams`),
+    response_time = as.numeric(RT),
+    tot_lanes = as.integer(`Lanes at Bottleneck`),
+    closed_lanes = as.integer(`Number of Lanes Closed`),
+    lane_ratio = closed_lanes / tot_lanes,
+    duration = as.numeric(`T7-T0 mod`),
+    peak = ifelse(str_detect(`Time Range`, "Off Peak"), "Off-peak", "Peak"),
+    tett = as.numeric(`Total Excess Travel Time`)) %>% 
+  drop_na(tett)
+  
 
 crash %>% 
-  select(-Date) %>% 
-  ggpairs(aes(color = Year), legend = c(1,1))
+  ggpairs(aes(color = year), legend = c(1,1))
 
 model <- crash %>% 
-  lm(
-    `Total Excess Travel Time` ~ Year + `Crash Type` + `Number of IMT Teams`,
-    data = .)
-
-crash %>% 
-  ggplot(aes(x = time, y = `Total Excess Travel Time`,
-             color = `Crash Type`)) +
-  geom_point() +
-  facet_wrap(~Year) +
-  scale_y_log10() +
-  scale_x_log10()
+  lm(tett ~ .^2, data = .)
 
 
-crash2 <- crash %>% 
-  transmute(Year, Time, `Number of IMT Teams`, `Crash Type`, `Response Time IMT`, `Affected Volume`, logtime = log(`Total Excess Travel Time`), roottime = sqrt(`Total Excess Travel Time`), open_lanes = `Lanes at Bottleneck` - `Number of Lanes Closed`, `Total Excess Travel Time`)
+step(model, direction = "both", k=log(278), trace = 0)
 
-crash2 %>% 
-  ggpairs(aes(color = Year), legend = c(1,1))
-
-crash %>% 
-  ggplot(aes(x = (`RCT for IMT`), y = (`Total Excess Travel Time`), color = Year, size = `Lanes at Bottleneck` - `Number of Lanes Closed`)) +
-  geom_point() +
-  geom_smooth()
+step(crash, scope = list(upper = tett ~ .^2, lower = tett ~ 1))
